0a7d5b8807da1583b5216850d93bfb396a01726f
ali <me74242@gmail.com>
2022-10-10 03:03:35 +0330

    ok !

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -13,6 +13,6 @@ export const inputArrayForUniversityInformation: I_Input[] = [
   { nameForShow: "نام استان محل تحصیل", name: "province", type: "text" },
   { nameForShow: "نام شهرستان محل تحصیل", name: "city", type: "text" },
   { nameForShow: "آخرین مدرک تحصیلی", name: "license", type: "text" },
-  { nameForShow: "معدل نهایی", name: "score", type: "text" },
+  { nameForShow: "معدل نهایی", name: "score", type: "number" },
   { nameForShow: "تصویر آخرین مدرک", name: "licenseImage", type: "file" },
 ];
